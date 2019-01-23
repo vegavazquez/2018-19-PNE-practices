50ecdf8ba870dfca74a16afb618e7d2dee85eb56
vegavazquez <vegavazquezardid@gmail.com>
2019-01-23 18:34:34 +0100

    just testig my first project in github

---
+++ PNE_session-2/hello.py
@@ -0,0 +1,13 @@
+print('hello')
+print('just texting the pucharm IDE')
+print('texting....2')
+
+
+
+print("printng the numvers from 1 to 20" )
+for i in range(20):
+    print(1+ i)
+total_sum=0
+for i in range[100]:
+    total_sum= total_sum + i + 1
+print('total sum is: '.format(total_sum)
