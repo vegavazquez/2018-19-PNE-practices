4b172c06d3df44559a6b697d5f7bcdb11ffd2325
vegavazquez <vegavazquezardid@gmail.com>
2019-02-05 16:33:23 +0100

    new file, objetos

---
+++ sesion- 5/seq_1.py
@@ -0,0 +1,30 @@
+class seq:
+    ''' a class for respresenting sequences'''
+    def __init__(self):
+        print(' new empty sequence created')
+
+        self.strbases= strbases
+    def len(self):
+        return len(self.strbases)
+
+
+
+s1= seq("AGTGCTAGAT")
+s2= seq('AGTCGATGCTA')
+
+l1= s1.len()
+l2= s2.len()
+
+
+str1= s1-strbases
+str2= s2.strbases
+
+print('sequence 1: {}'.format(str1))
+print('length sequence 1: { }'.format(l1))
+
+print('sequence 2: {}'.format(str2))
+print('lennght sequence 2: { }'. format(l2))
+
+
+
+print(' the end ')
