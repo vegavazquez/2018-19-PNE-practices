30bca78f3d53334191125d8d02f9953ad2ac55e5
vegavazquez <46930705+vegavazquez@users.noreply.github.com>
2019-03-09 15:30:12 +0100

    Add files via upload

---
+++ session13/webserver1.py
@@ -0,0 +1,21 @@
+#te coge los archivos de la carpeta donde estan el webserver1.py
+
+import http.server
+import socketserver
+
+# Define the Server's port
+PORT = 8000
+
+# -- Use the http.server Handler
+Handler = http.server.SimpleHTTPRequestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    httpd.serve_forever()
+
+
---
+++ session13/webserver2.3.py
@@ -0,0 +1,67 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8081
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        # Message to send back to the clinet
+        contents = "servidor de vega"
+
+
+        #SIEMPRE LO MISMO
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
